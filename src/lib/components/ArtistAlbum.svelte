8492a9c2d716bcc08f4ac9703d5885b958c13f0f
mappakev <mappakev@students.zhaw.ch>
2024-05-28 10:04:54 +0200

    album images

--- src/lib/components/ArtistAlbum.svelte
+++ src/lib/components/ArtistAlbum.svelte
@@ -11,7 +11,15 @@
   <div class="card my-4">
     <div class="card-body">
       <h2 class="card-title">{albums.name}</h2>
-      
+      <p class="card-text">
+        <a href={albums.external_urls.spotify}
+          >Link to Album</a
+        >
+      </p>
+      <p class="card-text">Release Date: {albums.release_date}
+      </p>
+      <!-- svelte-ignore a11y-img-redundant-alt -->
+      <img src={albums.images[1].url} alt="Album Image" />
     </div>
   </div>
   {/each}
